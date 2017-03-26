2f26fcc11ab5d6956bb5a426dbc7066643d89cbc
Stu <stu@drogna.org.uk>
2017-03-26 21:13:39 +0100

    only unmount what we need to

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 
-umount vm_build/*
+umount vm_build/proc
+umount vm_build/sys
+umount vm_build/dev
 umount vm_build/
 kpartx -d /dev/mapper/vgvm-lvfoo--vda
 lvremove /dev/vgvm/lvfoo-vda
