0ea26dcfd8ee9addb8caf309c40c387f1508ff0f
Stu <stu@drogna.org.uk>
2017-03-26 22:14:51 +0100

    make cleanup more generic

--- cleanup.sh
+++ cleanup.sh
@@ -4,5 +4,6 @@ umount vm_build/proc
 umount vm_build/sys
 umount vm_build/dev
 umount vm_build/
-kpartx -d /dev/mapper/vgvm-lvfoo--vda
-lvremove /dev/vgvm/lvfoo-vda
+virsh undefine $1
+kpartx -d /dev/mapper/vgvm-lv${1}--vda
+lvremove /dev/vgvm/lv${1}-vda
